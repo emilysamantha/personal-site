2d8cbc16c7763ee8eac3c5475e8c3a6168d51e15
emilysamantha <emilyzarry@gmail.com>
2023-01-19 12:49:24 +1100

    Undo adding Cards to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Hero from "./components/Hero";
-import Cards from "./components/Cards";
 
 function App() {
   return (
     <div className="App">
       <Hero />
-      <Cards />
     </div>
   );
 }
