6abfc601ab63b6bc643c2f41e66bb7f2139f7a36
emilysamantha <emilyzarry@gmail.com>
2023-01-19 16:41:36 +1100

    Add AOS to  Home

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
 import CardItem from "./CardItem";
 import "./CardItem.css";
-// import AOS from "aos";
-// import "aos/dist/aos.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Cards() {
-  //   useEffect(() => {
-  //     AOS.init({ once: true });
-  //   }, []);
+  useEffect(() => {
+    AOS.init({ once: true });
+  }, []);
 
   const projects = {
     "Sudoku Game": {
